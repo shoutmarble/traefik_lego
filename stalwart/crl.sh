73769c7c683ad79cd2dcedc5831fae6bc26507bb
ShoutMarble <terry.walsworth@outlook.com>
2025-08-04 15:16:28 -0400

    working stalwart mail server

--- stalwart/crl.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-curl -L http://who.landingdev.xyz/
-curl -L http://who.landingdev.xyz:80/
-curl -L http://who.landingdev.xyz:8080/
-curl -L http://who.landingdev.xyz:88/
-curl -L https://who.landingdev.xyz:443/
