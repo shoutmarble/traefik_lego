48e1f651aeb1b18b2b80fc1c6236605175eec0c0
ShoutMarble <terry.walsworth@outlook.com>
2025-08-06 00:50:43 -0400

    working stalwart...need to fix TLS only on JMAP and ADMIN

--- lego/manual.sh
+++ lego/manual.sh
@@ -4,7 +4,7 @@
 # .lego
 # ├── accounts
 # │   └── acme-v02.api.letsencrypt.org
-# │       └── twalsworth@gmail.com
+# │       └── xxxxxxxxxx@gmail.com
 # │           ├── account.json
 # │           └── keys
 # │               └── twalsworth@gmail.com.key
@@ -16,11 +16,11 @@
 # │   └── landingdev.xyz.pem
 # └── gen_keys.sh
 # 
-# COPY the "certifcates" from the ".lego" directory
+# COPY the certifcates foldeer from the ".lego" directory to a new "certs" directory
 # cp -fr .lego/certificates .
 # 
 # .
-# ├── certificates
+# ├── certs
 # │   ├── landingdev.xyz.crt
 # │   ├── landingdev.xyz.issuer.crt
 # │   ├── landingdev.xyz.json
@@ -36,15 +36,15 @@
 # 
 # STEP 2:
 # RUN
-./lego -a --pem --email "twalsworth@gmail.com" --domains "landingdev.xyz" --domains "*.landingdev.xyz" --dns manual run
+./lego -a --pem --email "xxxxxxxxx@gmail.com" --domains "landingdev.xyz" --domains "*.landingdev.xyz" --dns manual run
 # 
 # STEP 3:
-# COPY the "certifcates" from the ".lego" directory
-# cp -fr .lego/certificates .
+# COPY the "certifcates" folder  from the ".lego" directory to a new "certs" directory
+# cp -fr .lego/certificates/ certs
 
 # STEP 4:
 # OPEN URL https://toolbox.googleapps.com/apps/dig/#TXT/
 # and enter 
 # _acme-challenge.landingdev.xyz.
 #
-# verify TXT value matches what was entered "sOQfn3beSnqstpKRzInRUg-nu5WhczVZkeMAlUnKbcE"
+# verify TXT value matches what was entered in the DNS from LEGO "sOQf--------------------UnKbcE"
